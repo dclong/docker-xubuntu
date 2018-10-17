c46804180d41d405e475ea9eeefaff67dbb9b817
Ben Chuanlong Du <dclong@users.noreply.github.com>
2018-10-17 19:08:54 +0800

    Update 03-xubuntu.sh

--- scripts/03-xubuntu.sh
+++ scripts/03-xubuntu.sh
@@ -3,6 +3,6 @@
 export HOME=/home/$DOCKER_USER 
 mkdir -p $HOME/.config
 if [[ ! -d $HOME/.config/xfce4 ]]; then
-    mv /settings/xfce4 $HOME/.config/
+    # mv /settings/xfce4 $HOME/.config/
     chown -R $DOCKER_USER:$DOCKER_GROUP $HOME/.config/
 fi
